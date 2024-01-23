64f051ae666b5306eff6e91d0df10662ea8d753c
Trystan Cortez <trysyanmc22@gmail.com>
2024-01-23 17:38:55 -0600

    File structure

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const homeRoute = require('./routes/homeRoute');
+const authRoute = require('./routes/authRoute');
+const dashboardRoute = require('./routes/dashboardRoute');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const db = require('./models');
+
+// Routes
+app.use('/', homeRoute);
+app.use('/auth', authRoute);
+app.use('/dashboard', dashboardRoute);
+
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${PORT}`)
+    );
+});
